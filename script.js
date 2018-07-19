ca438b58ae431bf291cca9aa28ee3dc703af662d
Tommy <t@tommyhoang.com>
2018-07-19 13:25:30 -0700

    refactored code to include adding opacity

--- script.js
+++ script.js
@@ -6,45 +6,32 @@ function askUser () {
 }
 
 function makeBoxes(num) {
-	// let num = askUser();
 	container.setAttribute('style', `grid-template-columns: repeat(${num}, 1fr); grid-template-rows: repeat(${num}, 1fr);`);
 	for (let i = 0; i < num**2; i++) {
 		let box = document.createElement('div');
 		box.classList.add('box');
+		box.addEventListener('mouseover', (e) => {
+			makeDarker(e.target);
+		});
 		container.appendChild(box);
 	}
 }
 
-function colorBoxes() {
-	let boxes = document.querySelectorAll('.box');
-	boxes.forEach((box) => {
-		box.addEventListener('mouseover', () => {
-			box.classList.add('color1');	
-			if (box.getAttribute('class').includes('color1')) {
-				box.addEventListener('mouseover', () => {
-					box.classList.add('color2');
-				});
-			}
-			if (box.getAttribute('class').includes('color2')) { //bug, requires two mouseovers to add class color3
-				box.addEventListener('mouseover', () => {
-					box.classList.add('color3');
-				});
-			}
-			if (box.getAttribute('class').includes('color3')) {
-				box.classList.add('color4');
-			}
-		});
-	});
-
-};
+function makeDarker(target) {
+	//need to convert to number otherwise addition of string (target.style.opacity) with number gets hairy
+	let currentOpacity = Number(target.style.opacity); 
+	let opacIncrement = .2;
+	if (currentOpacity < 1) {
+		currentOpacity += opacIncrement;
+	} 
+	target.style.opacity = currentOpacity;
+}
 
 let resetBtn = document.querySelector('#reset');
 resetBtn.addEventListener('click', reset);
 function reset() {
 	container.innerHTML = '';
 	makeBoxes(askUser());
-	colorBoxes();
 }
 
 makeBoxes(24);
-colorBoxes();
